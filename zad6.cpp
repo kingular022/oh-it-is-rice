368f41969efe97e46bd9a26d52b2f4e9f097f313
kingular022 <92390717+kingular022@users.noreply.github.com>
2021-10-27 00:06:01 +0200

    Add files via upload

---
+++ zad6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool autobus, deszcz;
+	cout << "0 - nie, 1 - tak" << endl;
+
+	cout << "Czy pada deszcz? " << endl;
+	cin >> deszcz;
+	cout << "Czy autobus jest na przystanku? " << endl;
+	cin >> autobus;
+
+	if (deszcz && autobus)
+		cout << "Wez parasol, dostaniesz sie na uczelnie";
+	else if (deszcz && !autobus)
+		cout << "Nie dostaniesz sie na uczelnie";
+	else if (autobus && !deszcz)
+		cout << "Dostaniesz sie na uczelnie, milego dnia i pieknej pogody";
+	else
+		cout << "Nie dostaniesz sie na uczelnie, milego dnia i pieknej pogody ";
+}
+
---
+++ zad7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool znizka, premia;
+	cout << "0 - nie, 1 - tak" << endl;
+
+	cout << "Czy jest znizka na samochod?" << endl;
+	cin >> znizka;
+	cout << "Czy otrzymales premie?" << endl;
+	cin >> premia;
+
+	if (!znizka || premia)
+	{
+		cout << "Mozesz kupic samochod!" << endl;
+		cout << "Znizki na samochod nie ma" << endl;
+	}
+	else if (znizka || premia)
+		cout << "Mozesz kupic samochod!" << endl;
+	else if(znizka || !premia)
+		cout << "Jest znizka na samochod, mozesz go kupic" << endl;
+	else
+	{
+		cout << "Zakup samochodu trzeba odlozyc na pozniej ..." << endl;
+		cout << "Znizki na samochod nie ma" << endl;
+	}
+}
+
