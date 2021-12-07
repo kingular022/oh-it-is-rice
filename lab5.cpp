424db1f2d3d5ae18c7e31215560a7b2225e9670b
kingular022 <92390717+kingular022@users.noreply.github.com>
2021-12-07 23:40:58 +0100

    Add files via upload

---
+++ lab5.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+		//zad1 pierwsze jest ³añcuchowe, drugie zbiorcze
+		//zad2 taka inicjalizacja cin >> liczby; mozliwa jest tylko dla znaków
+		/*zad3
+		* 
+		char tab[] = "wiosna";
+		cout << size(tab);
+		
+		//zad5
+
+		char adres[400];
+		char nr_domu[50];
+		int ocena;
+		int staz;
+
+		cout << "Podaj nazwe ulicy na ktorej mieszkasz: ";
+		cin.getline(adres, 400);
+		cout << "podaj nr domu: ";
+		cin.getline(nr_domu, 50);
+		cout << "Na ile oceniasz swoje umiejetnosci programowania w skali [2-5]: ";
+		cin >> ocena;
+		cout << "Twoj staz programisty: ";
+		cin >> staz;
+
+		cout << "Adres: ";
+		cout << adres << " " << nr_domu << endl;
+		cout << "Ocena: ";
+		cout << ocena - 1 << endl;
+		cout << "Staz: ";
+		cout << staz << endl;
+	
+		//zad6
+
+		int sam = 0, spol = 0, znak = 0;
+		char tekst[200];
+
+		cout << "Wprowadz tekst malymi literami: ";
+		cin.getline(tekst, 200);
+
+		for (int i = 0; i < tekst[i] != '\0'; i++)
+		{
+			if (tekst[i] == 'a' || tekst[i] == 'e' || tekst[i] == 'i' || tekst[i] == 'o' || tekst[i] == 'u' || tekst[i] == 'y')
+				sam++;
+			else if (tekst[i] == ' ' || tekst[i] == '.' || tekst[i] == '!' || tekst[i] == '?' || tekst[i] == ';' || tekst[i] == ':')
+				znak++;
+			else
+				spol++;
+		}
+
+		cout << "Ilosc samoglosek w tekscie: " << sam << endl;
+		cout << "Ilosc spolglosek w tekscie: " << spol << endl;
+		*/
+}
